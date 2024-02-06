436c926ead0409f4dfd02a5a1d74d65501c52bfc
acunamatata82 <yar.oleg1982@mail.ru>
2024-02-06 03:41:33 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vue-courses/",
+  base: "/",
   plugins: [
     vue(), 
     svgLoader({
